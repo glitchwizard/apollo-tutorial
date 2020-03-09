f324e812f4dd3a53b5572dce35d639dc419ba2ef
Charley McGowan <glitchwizard@gmail.com>
2020-03-09 14:25:23 -0700

    Query resolvers created

--- start/server/src/index.js
+++ start/server/src/index.js
@@ -1,10 +1,23 @@
-const {
-    ApolloServer
-} = require('apollo-server');
+const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./schema');
+const { createStore } = require('./utils');
+const resolvers = require('./resolvers');
 
-const server = new ApolloServer({ typeDefs })
+const LaunchAPI = require('./datasources/launch');
+const UserAPI = require('./datasources/user');
+
+const store = createStore();
+
+const server = new ApolloServer({ 
+    typeDefs,
+    resolvers,
+    dataSources: () => ({
+        launchAPI: new LaunchAPI(),
+        userAPI: new UserAPI({ store })
+    })
+ });
 
 server.listen().then(({ url }) => {
     console.log(`Server Ready at ${url}`);
 });
+
